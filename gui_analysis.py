e5403ce6c2a8c50facdbc0f9d7513185ec5c0a3c
onemarshmallow <yukiyu859@gmail.com>
2023-03-04 22:58:51 -0500

    colors :)

--- gui_analysis.py
+++ gui_analysis.py
@@ -24,13 +24,21 @@ class WordTable(ct.CTkFrame):
         style = ttk.Style()
 
         style.configure('Treeview', fieldbackground='#343638', rowheight=40)
-        style.configure('Treeview.Heading', background="#343638", foreground='gray', font=('Helvetica', 20, 'bold'),
+        style.configure('Treeview.Heading', background="#343638", foreground='dark orchid', font=('Helvetica', 20, 'bold'),
                         fieldbackground='#343638')
 
         self.table = ttk.Treeview(self, columns=('Word', 'Frequency'), displaycolumns='#all',
                                   selectmode='none', show='headings')
 
-        self.table.insert('', 'end', 'widgets', text='Widget Tour')
+        self.table.insert('', 'end', 'widgets', text='Widget Tour', values=('sadness', 3), tags='ttk')
+        self.table.tag_configure('ttk', font=('Helvetica', 20, 'bold'), foreground='gray74', background='#343638')
+
+        # makes the headings appear
+        self.table.heading(0, text='Word')
+        self.table.heading(1, text='Frequency')
+        self.table.column(0, anchor='center')
+        self.table.column(1, anchor='center')
+
 
         # scrollbars
         self.scroll_x = ct.CTkScrollbar(self, orientation="horizontal", command=self.table.xview)
@@ -68,7 +76,7 @@ class Export(ct.CTkFrame):
 
         self.min_freq = ct.CTkEntry(self, placeholder_text='0', width=5)
         self.max_freq = ct.CTkEntry(self, placeholder_text='10', width=5)
-        self.export_button = ct.CTkButton(self, text='Export')
+        self.export_button = ct.CTkButton(self, text='Export', fg_color='medium orchid', border_color='purple1', hover_color='dark orchid')
 
         ct.CTkLabel(self, text='Export words with a frequency between').grid(row=0, column=0, columnspan=3, padx=20, pady=(20, 10))
         self.min_freq.grid(row=1, column=0, sticky='ew', padx=(30, 10))
@@ -76,7 +84,7 @@ class Export(ct.CTkFrame):
         self.max_freq.grid(row=1, column=2, sticky='ew', padx=(10, 30))
         self.export_button.grid(row=10, column=0, columnspan=3, pady=(30, 10))
 
-        # side note: i hate code. jk i mean i love code
+        # haha i love coding ( :
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
         self.rowconfigure(10, weight=1)
