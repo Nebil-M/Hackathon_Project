ce41e07c68d173b4015c32cb9cd32640c169592a
harshitattuluri <126976779+harshitattuluri@users.noreply.github.com>
2023-03-04 20:06:51 -0500

    additions

--- logic_controller.py
+++ logic_controller.py
@@ -2,7 +2,7 @@
 
 class Model:
     def __init__(self):
-        self.word_freq = {"rando": 1, "banana": 1, "apple": 1, "fruit": 1}
+        self.word_freq = {"rando": 8, "banana": 2, "apple": 4, "fruit": 5}
 
 
     def add_freq(self, word):
@@ -11,8 +11,10 @@ class Model:
         else:
             self.word_freq.update({word: 1})
 
-    def get_freq_range(self):
-        pass
+    def get_freq_range(self, minimum, maximum):
+        word_list = [word for word in self.word_freq.keys() if minimum <= self.word_freq[word] <= maximum]
+        return word_list
+
 
     def get_definition(self):
         pass
@@ -29,6 +31,7 @@ if __name__ == '__main__':
     m.add_freq('non')
     m.add_freq('non')
     print(m.word_freq)
+    print(m.get_freq_range(1, 5))
 
 
 
