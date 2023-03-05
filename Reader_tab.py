b3d816ff4d977127fd66d842afd64df2f8eaa8dc
Nebil <hiachi1118@gmail.com>
2023-03-04 22:16:59 -0500

    mains

--- Reader_tab.py
+++ Reader_tab.py
@@ -8,36 +8,46 @@ class ReaderTab(ct.CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Layout
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
 
         # Creating and griding
         text_read = TextRead(self)
-        text_read.grid(row=0, column=0, sticky='news')
+        text_read.grid(row=0, column=0, sticky='news', padx=10, pady=10)
 
         word = Word(self)
-        word.grid(row=0, column=1, sticky='news')
+        word.grid(row=0, column=1, sticky='news', padx=10, pady=10)
 
 
 # The left side
 class TextRead(ct.CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        #Layout
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
 
-        self.text = ct.CTkTextbox(self, wrap='word')
+        self.text = ct.CTkTextbox(self, wrap='word', padx=30, pady=30)
+        self.text.grid(row=0, column=0, sticky='news', padx=20, pady=10)
 
-        self.text.grid(row=0, column=0, sticky='news')
-
+        self.text.insert("0.0", "new text to insert")
 
 # The right side
 class Word(ct.CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Layout
+        #self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.columnconfigure(0, weight=1)
 
         self.word = tk.StringVar(value='word')
-        self.word_label = ct.CTkLabel(self, textvariable=self.word)
+        self.word_label = ct.CTkLabel(self, textvariable=self.word, font=('arial', 25))
         self.definition = ct.CTkTextbox(self, wrap='word')
 
-        self.word_label.grid(row=0, column=0)
-        self.definition.grid(row=1, column=0, sticky='ews')
+
+        self.word_label.grid(row=0, column=0, padx=10, pady=10)
+        self.definition.grid(row=1, column=0, sticky='news', padx=10, pady=10)
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import customtkinter as ct
+from tkinter import ttk
+import tkinter as tk
+from Reader_tab import ReaderTab
+from gui_analysis import AnalysisTab
 
 
-# Press the green button in the gutter to run the script.
+class controller:
+    def __init__(self):
+        pass
+
+
+class App(ct.CTkFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+        tabview = ct.CTkTabview(self)
+
+        tabview.add('Reader')
+        tabview.add('Analysis')
+        tabview.set('Reader')
+
+        tabview.grid(row=0, column=0, padx=10, pady=10, sticky='news')
+
+        r = ReaderTab(tabview.tab('Reader'))
+        r.grid(row=0, column=0, padx=10, pady=10, sticky='news')
+        a = AnalysisTab(tabview.tab('Analysis'))
+        a.grid(row=0, column=0, padx=10, pady=10, sticky='news')
+
+        # Layout
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
+
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    window = ct.CTk()
+    App(window).grid(row=0, column=0, sticky='news')
+    window.rowconfigure(0, weight=1)
+    window.columnconfigure(0, weight=1)
+    window.mainloop()
